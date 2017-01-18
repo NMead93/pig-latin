456e00d949301e565a8af615a2b5884821d413a1
grinil khanna and Nicholas Mead <grinil@>
2017-01-18 15:07:55 -0800

    Add functions which will include pig latin translation in Y and qu - - need to make everything case insensitive.

--- js/scripts.js
+++ js/scripts.js
@@ -4,21 +4,41 @@ $(document).ready(function() {
     var input = $('#request').val();
 
 
-    $('.output').text(translate(input));
+    $('.output').text(checkVowel(input));
   });
 });
 
 
-function translate(word) {
+function checkVowel(vowelWord) {
 
-  if ((word.includes("a")) || (word.includes("e")) || (word.includes("i")) || (word.includes("o")) || (word.includes("u"))) {
-    if ((word.charAt(0)) === "a" || (word.charAt(0)) === "e" || (word.charAt(0)) === "i" || (word.charAt(0)) === "o" || (word.charAt(0)) === "u") {
-      return word + "ay";
+  var nonRegex = vowelWord.replace(/[^a-zA-Z :]/g, "")
+
+  if ((nonRegex.toLowerCase().includes("a")) || (nonRegex.toLowerCase().includes("e")) || (nonRegex.toLowerCase().includes("i")) || (nonRegex.toLowerCase().includes("o")) || (nonRegex.toLowerCase().includes("u"))) {
+    if ((nonRegex.toLowerCase().charAt(0)) === "a" || (nonRegex.toLowerCase().charAt(0)) === "e" || (nonRegex.toLowerCase().charAt(0)) === "i" || (nonRegex.toLowerCase().charAt(0)) === "o" || (nonRegex.toLowerCase().charAt(0)) === "u") {
+      return nonRegex + "ay";
     } else  {
-        var i = 0
-        while (i !== "a") || ()
+        var i = 1
+        while ((nonRegex.charAt(i)) !== "a" && (nonRegex.charAt(i)) !== "e" && (nonRegex.charAt(i)) !== "i" && (nonRegex.charAt(i)) !== "o" && (nonRegex.charAt(i)) !== "u") {
+          i++;
+        }
+
+        if (nonRegex.toLowerCase().substring(i - 1, i + 1) === "qu") {
+          return checkQu(nonRegex, i);
+        } else {
+          return checkConstant(nonRegex, i);
+        }
     }
   } else {
     return "Please input a real word"
   }
+}
+
+function checkConstant(consanantWord, i) {
+  var consanant = consanantWord.substring(i, consanantWord.length) + consanantWord.substring(0, i) + "ay";
+  return consanant;
+}
+
+function checkQu(wordQu, i) {
+  var wordsWithQ = (wordQu.substring(i + 1, wordQu.length) + wordQu.substring(0, i + 1) + "ay");
+  return wordsWithQ;
 }
