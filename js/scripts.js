65d6b6b5a4e7baeaa153a15e4fc2a69f10786e32
grinil khanna and Nicholas Mead <grinil@>
2017-01-18 16:04:19 -0800

    Yield latin site which can translate all including sentences minus words with on y in it.

--- js/scripts.js
+++ js/scripts.js
@@ -3,22 +3,32 @@ $(document).ready(function() {
     event.preventDefault();
     var input = $('#request').val();
 
-
-    $('.output').text(checkVowel(input));
+    $('.output').text(sentenceCheck(input));
   });
 });
 
+function sentenceCheck(sentence) {
+  if (sentence.includes(" ")) {
+    var pigSentence = sentence.split(" ");
+    for (var i = 0; i < pigSentence.length; i++) {
+      pigSentence[i] = checkVowel(pigSentence[i]);
+    }
+  return pigSentence.join(" ");
+  } else {
+  return checkVowel(sentence);
+  }
+}
 
 function checkVowel(vowelWord) {
 
   var nonRegex = vowelWord.replace(/[^a-zA-Z :]/g, "")
 
-  if ((nonRegex.toLowerCase().includes("a")) || (nonRegex.toLowerCase().includes("e")) || (nonRegex.toLowerCase().includes("i")) || (nonRegex.toLowerCase().includes("o")) || (nonRegex.toLowerCase().includes("u"))) {
+  if ((nonRegex.toLowerCase().includes("a")) || (nonRegex.toLowerCase().includes("e")) || (nonRegex.toLowerCase().includes("i")) || (nonRegex.toLowerCase().includes("o")) || (nonRegex.toLowerCase().includes("u")) || (nonRegex.toLowerCase().includes("y"))) {
     if ((nonRegex.toLowerCase().charAt(0)) === "a" || (nonRegex.toLowerCase().charAt(0)) === "e" || (nonRegex.toLowerCase().charAt(0)) === "i" || (nonRegex.toLowerCase().charAt(0)) === "o" || (nonRegex.toLowerCase().charAt(0)) === "u") {
       return nonRegex + "ay";
     } else  {
         var i = 1
-        while ((nonRegex.charAt(i)) !== "a" && (nonRegex.charAt(i)) !== "e" && (nonRegex.charAt(i)) !== "i" && (nonRegex.charAt(i)) !== "o" && (nonRegex.charAt(i)) !== "u") {
+        while ((nonRegex.toLowerCase().charAt(i)) !== "a" && (nonRegex.toLowerCase().charAt(i)) !== "e" && (nonRegex.toLowerCase().charAt(i)) !== "i" && (nonRegex.toLowerCase().charAt(i)) !== "o" && (nonRegex.toLowerCase().charAt(i)) !== "u") {
           i++;
         }
 
